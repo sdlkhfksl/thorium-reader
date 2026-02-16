8713a570d48d9415b7118e9c876a5bca358eb3bb
Pierre Leroux <p.leroux@gmx.com>
2026-02-16 19:57:58 +0100

    fix(publicationIntegrityChecker): dump check results to app-logs directory

--- src/main/redux/sagas/publication/checker.ts
+++ src/main/redux/sagas/publication/checker.ts
@@ -12,12 +12,50 @@ import { diMainGet } from "readium-desktop/main/di";
 import * as fs from "node:fs";
 import * as path from "node:path";
 import { publicationExtensionStoredOnDisk } from "readium-desktop/common/extension";
+import { _APP_NAME, _APP_VERSION, _PACK_NAME } from "readium-desktop/preprocessor-directives";
+import { USER_DATA_FOLDER } from "readium-desktop/common/constant";
+
+// TODO: use app.getPath("logs"); instead
+const folderPath = path.join(
+    USER_DATA_FOLDER,
+    "app-logs",
+);
+const PROCESS_LOGS = "publicationsIntegrityChecker.txt";
+const appLogs = path.join(
+    folderPath,
+    PROCESS_LOGS,
+);
+
+if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath);
+}
+const processInfoStr = JSON.stringify({
+    node_version: process.version,
+    pid: process.pid,
+    platform: process.platform,
+    arch: process.arch,
+    uptime_seconds: process.uptime(),
+    memory_usage: process.memoryUsage(),
+    argv: process.argv,
+    MSWindowsStore: process.windowsStore,
+    thoriumAppName: _APP_NAME,
+    thoriumAppVersion: _APP_VERSION,
+    thoriumPackName: _PACK_NAME,
+    thoriumUserDataPath: USER_DATA_FOLDER,
+}, null, 4);
 
 // Logger
 const filename_ = "readium-desktop:main:redux:sagas:publication:checker";
-const debug = debug_(filename_);
+const debug__ = debug_(filename_);
 
 export function* publicationIntegrityChecker(): SagaGenerator<void> {
+
+    let dump = "#############################################\n";
+    dump += `Date: ${(new Date()).toISOString()}\n`;
+    const debug = (str: string) => {
+        debug__(str);
+        dump += str + "\n";
+    };
 
     const pubs = yield* selectTyped((state: RootState) => state.publication.db);
     const publicationDirectoryPath = yield* callTyped(() => diMainGet("publication-storage").getRootPath());
@@ -86,7 +124,7 @@ export function* publicationIntegrityChecker(): SagaGenerator<void> {
     }
     debug("--------");
 
-    debug(`${directoryFileCorrectArray.length} uuid directory found on disk and ${publicationDocumentIdArray.length} publication found in database`);
+    debug(`${directoryFileCorrectArray.length} uuid directory(ies) found on disk and ${publicationDocumentIdArray.length} publication(s) found in database`);
     debug(`${publicationDocumentNotFoundArray.length} publication(s) found in database but not found on the disk`);
     debug(`${publicationDocumentFoundArray.length} publication(s) matched between the database and the disk`);
 
@@ -102,6 +140,9 @@ export function* publicationIntegrityChecker(): SagaGenerator<void> {
             debug(`\tfileName: ${a.name}`);
         }
     }
-    debug(`seems to have ${directoryFileCorrectArray.length - filesInpublicationUUIDDirectoryNotCorrect.length} publications correct and identified in publication directory`);
+    debug(`seems to have ${directoryFileCorrectArray.length - filesInpublicationUUIDDirectoryNotCorrect.length} publication(s) correct and identified in publication directory`);
     debug("--------");
+
+    dump += `Process: ${processInfoStr}\n`;
+    fs.appendFileSync(appLogs, dump);
 }
