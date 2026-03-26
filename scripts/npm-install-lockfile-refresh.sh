8e438603cc7d74928166ec379ebf434808e161e2
Daniel Weck <daniel.weck@gmail.com>
2026-03-26 13:22:54 +0000

    chore(NPM): package dependencies updates, notably latest TypeScript with a few breaking changes (moving towards Go implementation which we have been aligning with for several months now, but WebPack's ts-loader needs a temporary patch until fixed upstream)

--- scripts/npm-install-lockfile-refresh.sh
+++ scripts/npm-install-lockfile-refresh.sh
@@ -3,8 +3,12 @@
 # https://www.npmjs.com/package/npm-scripts-lifecycle
 # https://app.unpkg.com/npm-scripts-lifecycle@1.0.0/files/package.json
 
+## TODO remove the local `npm run postinstall` once this is fixed:
+# https://github.com/TypeStrong/ts-loader/issues/1678
+# (remember to do the main.yaml GitHub Actions Workflow definition too, see `npm ci`)
+
 npm cache clear --force
-rm -rf node_modules/ && rm -f package-lock.json && npm install --ignore-scripts --foreground-scripts && npm run build:prod
+rm -rf node_modules/ && rm -f package-lock.json && npm install --ignore-scripts --foreground-scripts && npm run postinstall && npm run build:prod
 
 # npm install --foreground-scripts
 # TODO: preinstall, install and postinstall NPM lifecycle hooks for Electron, fsevents, ParcelWatcher, etc.?
