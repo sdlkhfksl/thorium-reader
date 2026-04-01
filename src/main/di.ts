cde5cc1baaff05ef804f40116d69894a6cc2369b
Pierre Leroux <panacpp@gmail.com>
2026-04-01 20:46:58 +0200

    refactor: JSON Application State Loading Strategy (PR #3471)

--- src/main/di.ts
+++ src/main/di.ts
@@ -39,6 +39,8 @@ import { tryCatch } from "readium-desktop/utils/tryCatch";
 import { EOL } from "os";
 import { FORCE_PROD_DB_IN_DEV, USER_DATA_FOLDER } from "readium-desktop/common/constant";
 import { PublicationData } from "./storage/publication-data";
+import { exec } from "node:child_process";
+import { getFileSize } from "readium-desktop/utils/fs";
 
 // import { streamer } from "readium-desktop/main/streamerHttp";
 // import { Server } from "@r2-streamer-js/http/server";
@@ -48,8 +50,6 @@ const debug = debug_("readium-desktop:main:di");
 
 export const CONFIGREPOSITORY_REDUX_PERSISTENCE = "CONFIGREPOSITORY_REDUX_PERSISTENCE";
 const capitalizedAppName = _APP_NAME.charAt(0).toUpperCase() + _APP_NAME.substring(1);
-
-import { exec } from "node:child_process";
 
 export let __ulimit_file: number;
 try {
@@ -86,16 +86,55 @@ export const stateFilePath = path.join(
     STATE_FILENAME,
 );
 
+try {
+    const stateFileSize = getFileSize(stateFilePath);
+    debug("INFO: state.json size is equal to", stateFileSize, "Octets");
+    if (stateFileSize < 1024) {
+        debug("\tIt seems to be a corrupted state, state.json size cannot be lower than 1kb");
+    }
+} catch {
+    debug("INFO: state.json not found");
+}
+
 const PATCH_FILENAME = "state.patch.json";
 export const patchFilePath = path.join(
     configDataFolderPath,
     PATCH_FILENAME,
 );
 
+try {
+    const patchFileSize = getFileSize(patchFilePath);
+    debug("INFO: state.patch.json size is equal to", patchFileSize, "Octets");
+} catch {
+    debug("INFO: state.patch.json not found");
+}
+
 const RUN_FILENAME = "state.runtime.json";
 export const runtimeStateFilePath = path.join(
     configDataFolderPath,
     RUN_FILENAME,
+);
+
+try {
+    const runtimeFileSize = getFileSize(runtimeStateFilePath);
+    debug("INFO: state.runtime.json size is equal to", runtimeFileSize, "Octets");
+    if (runtimeFileSize < 1024) {
+        debug("\tIt seems to be a corrupted state, state.runtime.json size cannot be lower than 1kb");
+    }
+} catch {
+    debug("INFO: state.runtime.json not found");
+}
+
+const RUN_DIFF_FILENAME = "state.runtime_patch.diff.json";
+export const runtimeDiffStateFilePath = path.join(
+    configDataFolderPath,
+    RUN_DIFF_FILENAME,
+);
+
+const STATE_DIFF_FILENAME = "state.diff.json";
+export const stateDiffFilePath = path.join(
+    configDataFolderPath,
+    STATE_DIFF_FILENAME,
 );
 
 // TODO: remove it for the next iteration
@@ -215,38 +254,38 @@ const getStorePromiseFn = async () => {
     // createStoreProcessLock.lock();
 
     debug("initStore");
-    const [store, sagaMiddleware] = await initStore();
-
-    // to test concurrent launch (one interactive app with lock, the other CLI)
-    // npm run start:dev (then close app, this is just to compile main.js)
-    // .. then launch 2 instances concurrently:
-    // npm run start:dev:main:electron -- opds Gallica "http://gallica.bnf.fr/opds" &
-    // npm run start:dev:main:electron &
-    // ...or the other way around:
-    // npm run start:dev:main:electron &
-    // npm run start:dev:main:electron -- opds Gallica "http://gallica.bnf.fr/opds" &
-    //
-    // to test long-running store initialisation:
-    // await new Promise((res) => setTimeout(() => res(undefined), 3*1000));
-
-    debug("store loaded");
-
-    container.bind<Store<RootState>>(diSymbolTable.store).toConstantValue(store);
-    container.bind<SagaMiddleware>(diSymbolTable["saga-middleware"]).toConstantValue(sagaMiddleware);
-    debug("container store and saga binded");
-
-    // createStoreProcessLock.release();
-
     try {
+        const [store, sagaMiddleware] = await initStore();
+
+        // to test concurrent launch (one interactive app with lock, the other CLI)
+        // npm run start:dev (then close app, this is just to compile main.js)
+        // .. then launch 2 instances concurrently:
+        // npm run start:dev:main:electron -- opds Gallica "http://gallica.bnf.fr/opds" &
+        // npm run start:dev:main:electron &
+        // ...or the other way around:
+        // npm run start:dev:main:electron &
+        // npm run start:dev:main:electron -- opds Gallica "http://gallica.bnf.fr/opds" &
+        //
+        // to test long-running store initialisation:
+        // await new Promise((res) => setTimeout(() => res(undefined), 3*1000));
+
+        debug("store loaded");
+
+        container.bind<Store<RootState>>(diSymbolTable.store).toConstantValue(store);
+        container.bind<SagaMiddleware>(diSymbolTable["saga-middleware"]).toConstantValue(sagaMiddleware);
+        debug("container store and saga binded");
+
+        // createStoreProcessLock.release();
+
         const state = diMainGet("store").getState();
         if (!state || typeof state !== "object") {
             throw new Error("state not defined : " + typeof state);
         }
+        return store;
     } catch (err) {
         const message = `REDUX STATE MANAGER CAN NOT BE INITIALIZED [${err}]${EOL}You should remove your 'AppData' folder${EOL}Thorium Exit code 1`;
         throw new Error(message);
     }
-    return store;
 };
 let getStorePromise: ReturnType<typeof getStorePromiseFn>;
 
